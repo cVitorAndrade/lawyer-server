11349bb07d3fac5ebbc5f197ab94ba81295608ee
cVitorAndrade <vitorandrade1804@gmail.com>
2025-02-28 18:17:40 -0300

    feat: encrypt password before create

--- src/modules/lawyer/use-cases/create-lawyer.use-case.ts
+++ src/modules/lawyer/use-cases/create-lawyer.use-case.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { LawyerRepository } from '../repositories/lawyer.repository';
 import { Lawyer } from '../entities/lawyer.entity';
+import { hash } from 'bcrypt';
 
 interface CreateLawyerRequest {
   name: string;
@@ -15,7 +16,11 @@ interface CreateLawyerRequest {
 export class CreateLawyerUseCase {
   constructor(private lawyerRepository: LawyerRepository) {}
   async execute(createLawyerRequest: CreateLawyerRequest) {
-    const lawyer = new Lawyer(createLawyerRequest);
+    const lawyer = new Lawyer({
+      ...createLawyerRequest,
+      password: await hash(createLawyerRequest.password, 10),
+    });
+
     await this.lawyerRepository.create(lawyer);
     return lawyer;
   }
