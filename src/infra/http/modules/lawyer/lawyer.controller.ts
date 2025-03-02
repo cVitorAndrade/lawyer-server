3e8410ee2417a435550dc79541dd0e7f8b184ec2
cVitorAndrade <vitorandrade1804@gmail.com>
2025-03-02 10:11:39 -0300

    feat: add get lawyer route

--- src/infra/http/modules/lawyer/lawyer.controller.ts
+++ src/infra/http/modules/lawyer/lawyer.controller.ts
@@ -1,16 +1,32 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request } from '@nestjs/common';
 import { CreateLawyerDto } from './dtos/create-lawyer.dto';
 import { CreateLawyerUseCase } from 'src/modules/lawyer/use-cases/create-lawyer.use-case';
 import { Public } from '../auth/decorators/isPublic';
+import { AuthenticatedRequestModel } from '../auth/models/authenticated-request.model';
+import { GetLawyerByIdUseCase } from 'src/modules/lawyer/use-cases/get-lawyer-by-id.use-case';
 
 @Controller('lawyer')
 export class LawyerController {
-  constructor(private createLawyerUseCase: CreateLawyerUseCase) {}
+  constructor(
+    private createLawyerUseCase: CreateLawyerUseCase,
+    private getLawyerByIdUseCase: GetLawyerByIdUseCase,
+  ) {}
 
   @Post()
   @Public()
   async createLawyer(@Body() body: CreateLawyerDto) {
     const lawyer = await this.createLawyerUseCase.execute(body);
+    return lawyer;
+  }
+
+  @Get()
+  async getLawyer(@Request() request: AuthenticatedRequestModel) {
+    const { user } = request;
+
+    const lawyer = await this.getLawyerByIdUseCase.execute({
+      lawyerId: user.id,
+    });
+
     return lawyer;
   }
 }
--- src/infra/http/modules/lawyer/lawyer.module.ts
+++ src/infra/http/modules/lawyer/lawyer.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { LawyerController } from './lawyer.controller';
 import { CreateLawyerUseCase } from 'src/modules/lawyer/use-cases/create-lawyer.use-case';
 import { DatabaseModule } from 'src/infra/database/database.module';
+import { GetLawyerByIdUseCase } from 'src/modules/lawyer/use-cases/get-lawyer-by-id.use-case';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [LawyerController],
-  providers: [CreateLawyerUseCase],
+  providers: [CreateLawyerUseCase, GetLawyerByIdUseCase],
 })
 export class LawyerModule {}
