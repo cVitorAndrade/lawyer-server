0ac18fb81923d5b8d36be61d91a4c0de4acb3a93
cVitorAndrade <vitorandrade1804@gmail.com>
2025-03-18 10:11:14 -0300

    feat: create client entity

---
+++ src/modules/client/entities/client.entity.ts
@@ -0,0 +1,73 @@
+import { randomUUID } from 'crypto';
+import { Replace } from 'src/utils/replace';
+
+interface ClientProps {
+  name: string;
+  email: string;
+  telephone: string;
+  birthDate: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export class Client {
+  private props: ClientProps;
+  private _id: string;
+
+  constructor(
+    props: Replace<ClientProps, { createdAt?: Date; updatedAt?: Date }>,
+    id?: string,
+  ) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+      updatedAt: props.updatedAt ?? new Date(),
+    };
+
+    this._id = id ?? randomUUID();
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get name(): string {
+    return this.props.name;
+  }
+
+  set name(name: string) {
+    this.props.name = name;
+  }
+
+  get email(): string {
+    return this.props.email;
+  }
+
+  set email(email: string) {
+    this.props.email = email;
+  }
+
+  get telephone(): string {
+    return this.props.telephone;
+  }
+
+  set telephone(telephone: string) {
+    this.props.telephone = telephone;
+  }
+
+  get birthDate(): Date {
+    return this.props.birthDate;
+  }
+
+  set birthDate(birthDate: Date) {
+    this.props.birthDate = birthDate;
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt;
+  }
+
+  get updatedAt(): Date {
+    return this.props.updatedAt;
+  }
+}
