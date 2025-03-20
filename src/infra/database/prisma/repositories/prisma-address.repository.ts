23ad8241a5bc745282a17c3953e28f488a087d0f
cVitorAndrade <vitorandrade1804@gmail.com>
2025-03-19 21:33:51 -0300

    feat: create address repository

---
+++ src/infra/database/prisma/repositories/prisma-address.repository.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { Address } from 'src/modules/address/entitites/address.entity';
+import { AddressRepository } from 'src/modules/address/repositories/address.repository';
+import { PrismaAddressMapper } from '../mappers/prisma-address.mapper';
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class PrismaAddressRepository implements AddressRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(address: Address): Promise<void> {
+    const prismaAddress = PrismaAddressMapper.toPrisma(address);
+    await this.prismaService.addresses.create({
+      data: prismaAddress,
+    });
+  }
+}
