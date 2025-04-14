2c5908d25a77548d7449fcdc0e9b41ab6dc0ff1d
cVitorAndrade <vitorandrade1804@gmail.com>
2025-04-14 14:06:24 -0300

    feat: add domain when return cookie

--- src/infra/http/modules/auth/auth.controller.ts
+++ src/infra/http/modules/auth/auth.controller.ts
@@ -35,6 +35,8 @@ export class AuthController {
       secure: true,
       sameSite: 'none',
       maxAge: tokenExpiresInMs,
+      domain: 'lawyer-app-server.onrender.com',
+      path: '/',
     });
 
     return { access_token };
