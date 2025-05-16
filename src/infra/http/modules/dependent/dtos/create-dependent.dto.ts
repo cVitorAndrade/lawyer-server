c320c338d3001976d3680b7f0f3dfb4e4c4f1fa3
cVitorAndrade <vitorandrade1804@gmail.com>
2025-05-16 17:46:29 -0300

    feat: add create dependent dto

---
+++ src/infra/http/modules/dependent/dtos/create-dependent.dto.ts
@@ -0,0 +1,50 @@
+import { Type } from 'class-transformer';
+import { IsDate, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateDependentDto {
+  @IsString()
+  @IsNotEmpty()
+  clientId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  telephone: string;
+
+  @IsString()
+  @IsNotEmpty()
+  rg: string;
+
+  @IsString()
+  @IsNotEmpty()
+  cpf: string;
+
+  @IsString()
+  @IsNotEmpty()
+  maritalStatus: string;
+
+  @IsString()
+  @IsNotEmpty()
+  gender: string;
+
+  @IsString()
+  @IsNotEmpty()
+  motherName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  occupation: string;
+
+  @IsDate()
+  @IsNotEmpty()
+  @Type(() => Date)
+  birthDate: Date;
+}
