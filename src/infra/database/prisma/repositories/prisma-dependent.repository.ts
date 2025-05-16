5d2b055685a5c9d1ce8fdd31d62a51e5e146d2c5
cVitorAndrade <vitorandrade1804@gmail.com>
2025-05-16 17:23:06 -0300

    feat: implements the create method in the prisma dependent repository

---
+++ src/infra/database/prisma/repositories/prisma-dependent.repository.ts
@@ -0,0 +1,17 @@
+import { DependentRepository } from 'src/modules/dependent/repositories/dependent.repository';
+import { PrismaService } from '../prisma.service';
+import { Dependent } from 'src/modules/dependent/entities/dependent.entity';
+import { Injectable } from '@nestjs/common';
+import { PrismaDependentMapper } from '../mappers/prisma-dependent.mapper';
+
+@Injectable()
+export class PrismaDependentRepository implements DependentRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(dependent: Dependent): Promise<void> {
+    const prismaDependent = PrismaDependentMapper.toPrisma(dependent);
+    await this.prismaService.dependents.create({
+      data: prismaDependent,
+    });
+  }
+}
